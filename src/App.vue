e748f813ac83029dcb0b9234e9e857bdc4dcbbba
eleCush <v1nc3ntpull1ng@gmail.com>
2024-03-20 23:51:04 -0600

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <LandingPage />
+  </div>
+</template>
+
+<script>
+import LandingPage from './components/LandingPage.vue';
+
+export default {
+  name: 'App',
+  components: {
+    LandingPage
+  }
+};
+</script>
