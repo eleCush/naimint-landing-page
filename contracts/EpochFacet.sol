0e524bd013f849a34e03b921393bc87d78635278
eleCush <v1nc3ntpull1ng@gmail.com>
2024-03-23 01:31:21 -0600

    even rewards

--- contracts/EpochFacet.sol
+++ contracts/EpochFacet.sol
@@ -6,22 +6,13 @@ import "./LibDiamond.sol";
 contract EpochFacet {
     event EpochStarted(uint256 indexed epochId, uint256 startTime);
     event EpochEnded(uint256 indexed epochId, uint256 endTime);
+    event RewardDistributed(uint256 indexed submissionId, uint256 rewardAmount);
 
     function diamondStorage() internal pure returns (LibDiamond.DiamondStorage storage ds) {
         assembly {
             ds.slot := 0
         }
     }
-
-
-    function prepareForNextEpoch() internal {
-       LibDiamond.DiamondStorage storage ds = diamondStorage();
-       ds.totalVotes = 0;
-       ds.totalLinkSubmissions = 0;
-       //ds.previousAverageUpvotes = ds.averageUpvotes; //optionally save the previous one to a value before clearing
-       ds.averageUpvotes = 0;
-    }
-    
 
     function startEpoch() external {
         LibDiamond.enforceIsContractOwner();
@@ -30,39 +21,61 @@ contract EpochFacet {
 
         ds.epochId++;
         ds.epochStartTime = block.timestamp;
-        ds.epochEndTime = block.timestamp + 4 days;
+        ds.epochEndTime = block.timestamp + 44 minutes; // For testing, use 44 minutes as the epoch duration
 
         emit EpochStarted(ds.epochId, ds.epochStartTime);
     }
-
 
     function getCurrentEpoch() external view returns (uint256) {
         return diamondStorage().epochId;
     }
 
-   
-   function finalizeEpoch() external {
-    LibDiamond.DiamondStorage storage ds = diamondStorage();
-    require(block.timestamp >= ds.epochEndTime, "EpochFacet: Epoch not ended yet");
-    
-    uint256 totalRewards = ds.rewardPool; // Assuming this is tracked in DiamondStorage
-    //actually totalRewards will be 121.68 every time, but maybe with an on-ramp for the first ~77 epochs
-    uint256 totalSubmissions = ds.postCount; // Assuming this is tracked in DiamondStorage
-    uint256 totalVotes = ds.voteCount; // Assuming this is tracked in DiamondStorage
-    uint256 averageVotes = totalVotes / totalSubmissions;
 
-    // Directly distribute rewards to eligible submissions
-    for(uint256 i = 0; i < totalSubmissions; i++) {
-        if(ds.posts[i].voteCount > averageVotes) {
-            uint256 rewardAmount = calculateRewardAmount(ds.posts[i].voteCount, totalRewards, totalSubmissions, averageVotes);
-            distributeReward(ds.posts[i].author, rewardAmount);
-            emit RewardDistributed(i, rewardAmount);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Using the total number of Votes and total number of Links Submitted, we find the average Upvote amount
+//we iterate over the whole collection of links and award an even split of the winnings  to any link with > average upvotes
+//see note below
+    function finalizeEpoch() external {
+        LibDiamond.DiamondStorage storage ds = diamondStorage();
+        require(block.timestamp >= ds.epochEndTime, "EpochFacet: Epoch not ended yet");
+        
+        uint256 totalRewards = 121.68 ether; // Total rewards for the epoch
+        uint256 totalSubmissions = ds.postCount;
+        uint256 totalVotes = ds.voteCount;
+        uint256 averageVotes = totalVotes / totalSubmissions;
+        uint256 rewardAmount = totalRewards / totalSubmissions; // Evenly distributed
+
+        // Directly distribute rewards to eligible submissions
+        for(uint256 i = 0; i < totalSubmissions; i++) {
+            if(ds.posts[i].voteCount > averageVotes) {
+                distributeReward(ds.posts[i].author, rewardAmount);
+                emit RewardDistributed(i, rewardAmount);
+            }
         }
+
+        prepareForNextEpoch();
     }
 
-    // Reset state for next epoch
-    prepareForNextEpoch();
-   }
 
    /* This brilliantly simple setup, courtesy of yours truly, will pay out over time to the top 50% of content submissions.
       Some specific epochs might have the top 70% remunerated or the top 25% remunerated because their number of votes for each of those qualifying submissions exceeds the average vote count.
@@ -79,9 +92,23 @@ contract EpochFacet {
       -VSP*/
 
 
-      // Utility function to check if the current epoch has ended
-      function isEpochEnd() public view returns (bool) {
+
+
+    function prepareForNextEpoch() internal {
         LibDiamond.DiamondStorage storage ds = diamondStorage();
-        return block.timestamp >= ds.epochEndTime;
+        ds.epochId++;
+        ds.epochStartTime = block.timestamp;
+        ds.epochEndTime = block.timestamp + 44 minutes; // For testing
+        ds.postCount = 0;
+        ds.voteCount = 0;
+        // Reset posts and votes for the new epoch
+        // Implement logic to reset posts and votes
+        ds.Posts = []; //will this wokr?
+        ds.Votes = []; //???
+    }
+
+    // Placeholder for reward distribution logic
+    function distributeReward(address recipient, uint256 rewardAmount) internal {
+        // Implement reward distribution logic, possibly calling the TokenFacet to transfer tokens
     }
 }
